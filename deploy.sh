e8f630d302954909ce36fd6ed08e7a7d28caa763
Marti <m4rti21@proton.me>
2025-07-19 13:20:50 +0200

    install

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-hostname=$1
-
-# script to deploy systems
 sudo nix \
     --experimental-features "nix-command flakes" \
     run github:nix-community/disko -- \
     --mode disko ./modules/system/disko.nix
 
-sudo nixos-install --root /mnt --flake ./#$hostname
+sudo nixos-install --root /mnt --flake ./#lounge
 
 echo "instalation done! you may now reboot"

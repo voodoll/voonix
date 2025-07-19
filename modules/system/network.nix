c9e78c2bbcb79b6facf66d022c0ff1cd65763a7d
Marti <m4rti21@proton.me>
2025-07-19 15:56:52 +0200

    a

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -13,13 +13,5 @@
         # ...
       ];
     };
-    defaultGateway = "192.168.40.1";
-    nameservers = ["1.1.1.1" "8.8.8.8"];
-    interfaces.eth0.ipv4.addresses = [
-      {
-        address = "192.168.40.200";
-        prefixLength = 24;
-      }
-    ];
   };
 }
